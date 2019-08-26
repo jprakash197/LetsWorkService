1f45ad5a11c0c14920f9e5906b5f1e14a163af89
Jyoti Prakash Behera <jyoti.behera@mindtree.com>
2019-08-26 21:05:35 +0530

    changed properties as per profile

--- src/main/java/com/mindtree/letswork/property/LetsWorkProperty.java
+++ src/main/java/com/mindtree/letswork/property/LetsWorkProperty.java
@@ -1,5 +1,6 @@
 package com.mindtree.letswork.property;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 /**
@@ -7,40 +8,44 @@ import org.springframework.stereotype.Component;
  *
  */
 @Component
+@ConfigurationProperties(prefix = "app")
 public class LetsWorkProperty {
-	
-	private String title; 
-	private String project; 
-	private String version; 
+
+	private String title;
+	private String project;
+	private String version;
 	private String environment;
-	
-	
+
 	public String getTitle() {
 		return title;
 	}
+
 	public String getProject() {
 		return project;
 	}
+
 	public String getVersion() {
 		return version;
 	}
-	
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public void setProject(String project) {
-		this.project = project;
-	}
-	public void setVersion(String version) {
-		this.version = version;
-	}
+
 	public String getEnvironment() {
 		return environment;
 	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public void setProject(String project) {
+		this.project = project;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
 	public void setEnvironment(String environment) {
 		this.environment = environment;
 	}
-	
-
 
 }
