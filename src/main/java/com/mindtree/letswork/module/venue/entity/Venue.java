a030beb3e642e03f693460baba61f401c3a87bd7
Apoorva R <M1053722@mindtree.com>
2019-09-03 18:41:39 +0530

    updated venue and image entity

--- src/main/java/com/mindtree/letswork/module/venue/entity/Venue.java
+++ src/main/java/com/mindtree/letswork/module/venue/entity/Venue.java
@@ -2,8 +2,10 @@ package com.mindtree.letswork.module.venue.entity;
 
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -21,42 +23,44 @@ public class Venue {
 
 	@Column
 	private String venueName;
-	
+
 	@Column
 	private String city;
-	
+
 	@Column
 	private String address;
-	
+
 	@Column
 	private double size;
-	
+
 	@Column
 	private int capacity;
-	
+
 	@Column
 	private String description;
-	
+
 	@Column
 	private int rating;
-	
+
 	@Column
 	private double price;
-	
+
 	@Column
 	private String venueType;
 
-	@OneToMany(mappedBy="venue")
+	@OneToMany(mappedBy = "venue")
 	private Set<Booking> bookings;
-	
+
+	@OneToMany(fetch = FetchType.EAGER)
+	private Set<Images> image;
+
 	public Venue() {
 		super();
 	}
 
-	public Venue(int venueId, String venueName, String city, String address, double size, int capacity,
-			String description, int rating, double price, String venueType) {
+	public Venue(String venueName, String city, String address, double size, int capacity, String description,
+			int rating, double price, String venueType, Set<Booking> bookings, Set<Images> image) {
 		super();
-		this.venueId = venueId;
 		this.venueName = venueName;
 		this.city = city;
 		this.address = address;
@@ -66,6 +70,8 @@ public class Venue {
 		this.rating = rating;
 		this.price = price;
 		this.venueType = venueType;
+		this.bookings = bookings;
+		this.image = image;
 	}
 
 	public int getVenueId() {
@@ -155,4 +161,92 @@ public class Venue {
 	public void setBookings(Set<Booking> bookings) {
 		this.bookings = bookings;
 	}
+
+	public Set<Images> getImage() {
+		return image;
+	}
+
+	public void setImage(Set<Images> image) {
+		this.image = image;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + ((bookings == null) ? 0 : bookings.hashCode());
+		result = prime * result + capacity;
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((image == null) ? 0 : image.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(price);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + rating;
+		temp = Double.doubleToLongBits(size);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + venueId;
+		result = prime * result + ((venueName == null) ? 0 : venueName.hashCode());
+		result = prime * result + ((venueType == null) ? 0 : venueType.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Venue other = (Venue) obj;
+		if (address == null) {
+			if (other.address != null)
+				return false;
+		} else if (!address.equals(other.address))
+			return false;
+		if (bookings == null) {
+			if (other.bookings != null)
+				return false;
+		} else if (!bookings.equals(other.bookings))
+			return false;
+		if (capacity != other.capacity)
+			return false;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (image == null) {
+			if (other.image != null)
+				return false;
+		} else if (!image.equals(other.image))
+			return false;
+		if (Double.doubleToLongBits(price) != Double.doubleToLongBits(other.price))
+			return false;
+		if (rating != other.rating)
+			return false;
+		if (Double.doubleToLongBits(size) != Double.doubleToLongBits(other.size))
+			return false;
+		if (venueId != other.venueId)
+			return false;
+		if (venueName == null) {
+			if (other.venueName != null)
+				return false;
+		} else if (!venueName.equals(other.venueName))
+			return false;
+		if (venueType == null) {
+			if (other.venueType != null)
+				return false;
+		} else if (!venueType.equals(other.venueType))
+			return false;
+		return true;
+	}
+
 }
