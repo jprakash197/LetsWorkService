2738945af42f63470307a9c57013e6fe0b1b1fcc
Rohit <rohit.kumar7@mindtree.com>
2019-09-04 21:44:27 +0530

    modified pom.xml and added prefix and suffix in application properties and added a letsworkindex.jsp to backend

--- src/main/java/com/mindtree/letswork/HomeController.java
+++ src/main/java/com/mindtree/letswork/HomeController.java
@@ -1,21 +1,22 @@
 package com.mindtree.letswork;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.mindtree.letswork.property.LetsWorkProperty;
 
-@RestController
+@Controller
 @CrossOrigin
 public class HomeController {
 	@Autowired
 	private LetsWorkProperty letsWorkProperty; 
 	
-	@GetMapping("/")
-	public String home() {
-		return letsWorkProperty.toString();
-	}
 	
+	@GetMapping("/")
+	public String home(ModelMap model) {
+		model.addAttribute("work", letsWorkProperty);
+		return "letsworkindex";
+	}
 }
--- src/main/webapp/WEB-INF/view/letsworkindex.jsp
+++ src/main/webapp/WEB-INF/view/letsworkindex.jsp
@@ -4,12 +4,21 @@
 <!DOCTYPE html>
 <html>
 <head>
+<style>
+body {background-color: powderblue;}
+h1   {color: blue;font: 300%;text-align:center}
+h2   {color: blue;font: 180%;}
+p    {color: red;font: 100%;}
+</style>
 <meta charset="ISO-8859-1">
-<title>Insert title here</title>
+<title>LET'S WORK</title>
 </head>
 <body>
-	<h1>Let's Work</h1>
-	<p>i am here</p>
-
+    <h1>WELCOME TO LET'S WORK</h1><br><br>
+	<h2>LET'S WORK PROPERTIES</h2><br>
+	<p>TITLE &nbsp; : &nbsp; ${work.title}</p>
+	<p>PROJECT &nbsp; : &nbsp; ${work.project}</p>
+    <p>VERSION  &nbsp; : &nbsp; ${work.version}</p>
+    <p>ENVIRONMENT &nbsp; : &nbsp; ${work.environment}</p>
 </body>
 </html>
